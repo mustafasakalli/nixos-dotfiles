76f3015b9d70fa1ae1f32a5edc44366615aacfff
mustafasakalli <mustafasakalli42@gmail.com>
2026-07-02 16:31:07 +0300

    separate zsh.nix

--- home.nix
+++ home.nix
@@ -13,35 +13,12 @@
   imports = [
     ./modules/alacritty.nix
     ./modules/rofi.nix
+    ./modules/zsh.nix
   ];
 
   # Qtile Ayar Dosyasını Otomatik Bağlama
   xdg.configFile."qtile/config.py".source = ./qtile/config.py;
-  
-  # ZSH Yapılandırması
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true; 
-    autosuggestion.enable = true; 
-    syntaxHighlighting.enable = true; 
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "sudo" ];
-      theme = "robbyrussell"; 
-    };
-
-    shellAliases = {
-      ll = "ls -l";
-      nrs = "sudo nixos-rebuild switch --flake ~/dotfiles#nixos"; 
-      nfu = "nix flake update ~/dotfiles";                        
-      qtile-bak = "geany ~/dotfiles/qtile/config.py";    
-      gs  = "git status";
-	  gcm = "git commit -m";
-      ga  = "git add .";          
-    };
-  };
-  
+    
   home.packages = with pkgs; [
     tree
     bat
---
+++ modules/zsh.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }: {
+    
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true; 
+    autosuggestion.enable = true; 
+    syntaxHighlighting.enable = true; 
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "sudo" ];
+      theme = "robbyrussell"; 
+    };
+
+    shellAliases = {
+      ll = "ls -l";
+      nrs = "sudo nixos-rebuild switch --flake ~/dotfiles#nixos"; 
+      nfu = "nix flake update ~/dotfiles";                        
+      qtile-bak = "geany ~/dotfiles/qtile/config.py";    
+      gs  = "git status";
+      gcm = "git commit -m";
+      ga  = "git add .";          
+    };
+  };
+}
