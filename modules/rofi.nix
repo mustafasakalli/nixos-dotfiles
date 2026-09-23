a6e38ecf2653f12273ceeee87ecc8a13e27c9dd3
mustafasakalli <mustafasakalli42@gmail.com>
2026-09-23 19:27:41 +0300

    rofi nix dosyasından tırnakları kaldır. hataya sebep oluyor

--- modules/rofi.nix
+++ modules/rofi.nix
@@ -4,25 +4,20 @@
   programs.rofi = {
     enable = true;
 
-    # Stable sürümde 'settings' yok; 'extraConfig' kullanılır.
-    # Tüm değerler string olarak yazılmalı, çünkü rasi config.rasi'ye gömülür.
     extraConfig = {
-      terminal             = "${pkgs.alacritty}/bin/alacritty";
-      modi                 = "run,drun,window";
-      icon-theme           = "Papirus-Dark";
-      show-icons           = "true";
-      drun-display-format  = "{icon} {name}";
-      location             = "0";
-      disable-history      = "false";
-      hide-scrollbar       = "true";
-      display-drun         = "    Apps ";
-      display-run          = "    Run ";
-      display-window       = " 󰕰  Window ";
-      sidebar-mode         = "true";
+      terminal            = "${pkgs.alacritty}/bin/alacritty";
+      modi                = "run,drun,window";
+      icon-theme          = "Papirus-Dark";
+      show-icons          = true;   # Tırnak kaldırıldı (boolean)
+      drun-display-format = "{icon} {name}";
+      location            = 0;      # Tırnak kaldırıldı (integer)
+      disable-history     = false;  # Tırnak kaldırıldı (boolean)
+      sidebar-mode        = false;  # Tırnak kaldırıldı (boolean)
+      display-drun        = "    Apps ";
+      display-run         = "    Run ";
+      display-window      = " 󰕰  Window ";
     };
 
-    # Stable sürümde 'theme' bir dosya yolu bekler.
-    # Ayrı .rasi dosyası olarak yazdık.
     theme = ./rofi-theme.rasi;
   };
 }
